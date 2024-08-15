e9776d41732a2d9a6d5a621a0503b5fd081e8e22
AZZAM UDDIN <uazzam69@gmail.com>
2024-08-16 01:12:14 +0530

    updated schema

--- primary-backend/src/types/index.ts
+++ primary-backend/src/types/index.ts
@@ -1,8 +1,8 @@
 import {z} from 'zod';
 
 export const SignupSchema = z.object({
-    username: z.string(),
-    password: z.string()
+    username: z.string().min(5),
+    password: z.string().min(6)
 });
 
 // export default SignupData = z.object({
